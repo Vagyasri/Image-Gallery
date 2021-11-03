bc4ba0ba7d2f10fa34bee77360a5407d0be55d85
Vagyasri <vagyasri2011@gmail.com>
2021-11-03 10:37:17 +0100

    Make it functional for the middlesection only

--- src/index.js
+++ src/index.js
@@ -1,16 +1,9 @@
 import './style.css';
 
 const pictures = document.querySelectorAll('.pic');
-const quotes = document.querySelectorAll('p');
 
 const toggleOpen = (e) => {
-  e.target.classList.toggle('open');
-};
-const toggleActive = (e) => {
-  if (e.propertyName === 'flex-grow') {
-    e.target.parentElement.classList.toggle('open-active');
-  }
+  e.target.parentElement.classList.toggle('open');
 };
 
 pictures.forEach((picture) => picture.addEventListener('click', toggleOpen));
-quotes.forEach((quote) => quote.addEventListener('transitionend', toggleActive));
