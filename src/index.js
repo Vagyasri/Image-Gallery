588af7a763d8b2537476af7febdd470ab9855416
Vagyasri <vagyasri2011@gmail.com>
2021-11-03 10:41:17 +0100

    Make it functional for all the sections

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const pictures = document.querySelectorAll('.pic');
 
 const toggleOpen = (e) => {
   e.target.parentElement.classList.toggle('open');
+  e.target.classList.toggle('open');
 };
 
 pictures.forEach((picture) => picture.addEventListener('click', toggleOpen));
